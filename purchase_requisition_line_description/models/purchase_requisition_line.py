1a5ab6a22dabc15bfd6cd2ffa05af4a9bfccbcec
Ruchir Shukla <ruchir@bizzappdev.com>
2025-04-21 16:25:37 +0530

    [MIG] purchase_requisition_line_description: Migration to 18.0

--- purchase_requisition_line_description/models/purchase_requisition_line.py
+++ purchase_requisition_line_description/models/purchase_requisition_line.py
@@ -8,11 +8,15 @@ from odoo.fields import first
 class PurchaseRequisitionLine(models.Model):
     _inherit = "purchase.requisition.line"
 
-    name = fields.Text(string="Description")
+    name = fields.Text(string="Product Description")
 
     @api.onchange("product_id")
     def _onchange_product_id(self):
-        res = super()._onchange_product_id()
+        """
+        New method is triggered when the `product_id` field is changed.
+        It updates the `name` field (description) based on the selected product
+        and the partner's language and purchase description.
+        """
         if self.product_id:
             partner = (
                 self.requisition_id.purchase_ids.partner_id
@@ -25,4 +29,3 @@ class PurchaseRequisitionLine(models.Model):
             self.name = product_lang.display_name
             if product_lang.description_purchase:
                 self.name += "\n" + product_lang.description_purchase
-        return res
